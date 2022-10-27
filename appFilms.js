d96e25fe5de20005492afc00aa54b310e9384e9a
Vladimir <vladimir@yakimkov.ru>
2022-10-27 23:09:26 +0300

    Add function

--- appFilms.js
+++ appFilms.js
@@ -11,41 +11,47 @@ genres
 
 P.S. Функции вызывать не обязательно*/
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
 const personalMovieDB = {
     count: numberOfFilms ,
-    movies: {
-        
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
-// const firstAnswerLastFilm = prompt('Один из последних просмотренных фильмов?', 'Хищник');
-// const firstScoreFilm = +prompt('На сколько оцените его?', '9');
-// const secondAnswerLastFilm = prompt('Один из последних просмотренных фильмов?', 'Соник');
-// const secondScoreFilm = +prompt('На сколько оцените его?', '8');
-// personalMovieDB.movies[firstAnswerLastFilm] = firstScoreFilm;
-// personalMovieDB.movies[secondAnswerLastFilm] = secondScoreFilm;
 
-for(let i = 0; i < 2; i++) {
-    const firstAnswerLastFilm = prompt('Один из последних просмотренных фильмов?', '');
-    const firstScoreFilm = +prompt('На сколько оцените его?', '');
-
-    if(firstAnswerLastFilm != null && firstScoreFilm != null && firstAnswerLastFilm != '' && firstScoreFilm != '' && firstAnswerLastFilm.length < 50  ) {
-        personalMovieDB.movies[firstAnswerLastFilm] = firstScoreFilm;
-    } else {i--;}
+function rememberMyFilms () {
+    for(let i = 0; i < 2; i++) {
+        const firstAnswerLastFilm = prompt('Один из последних просмотренных фильмов?', '');
+        const firstScoreFilm = +prompt('На сколько оцените его?', '');
     
+        if(firstAnswerLastFilm != null && firstScoreFilm != null && firstAnswerLastFilm != '' && firstScoreFilm != '' && firstAnswerLastFilm.length < 50  ) {
+            personalMovieDB.movies[firstAnswerLastFilm] = firstScoreFilm;
+        } else {i--;}
+        
+    }
 }
-if(personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов")
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    alert("Вы классический зритель");
-} else if(personalMovieDB.count >= 30) {
-    alert("Вы классический зритель");
-} else {
-    alert("Произошла ошибка");
-};
+rememberMyFilms();
 
+function detectPersonalLevel () {
+    if(personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов")
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        alert("Вы классический зритель");
+    } else if(personalMovieDB.count >= 30) {
+        alert("Вы классический зритель");
+    } else {
+        alert("Произошла ошибка");
+    };
+}
+detectPersonalLevel ();
 // Проверить, чтобы все работало без ошибок в консоли
 console.log(personalMovieDB);
